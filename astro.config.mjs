208087b2dbe66edb339b6d8c265f3089578f7a56
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-04-19 18:43:09 +0100

    website fixed :D

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import path from 'node:path';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'OmgRod',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
 		}),
 	],
+	vite: {
+		resolve: {
+			alias: {
+				'@': path.resolve('./src'),
+			}
+		}
+	},
 });
