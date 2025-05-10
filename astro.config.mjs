00b3880175773584a8aab79ebf5ac692092db33a
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-05-10 09:02:27 +0100

    more info

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,30 +3,31 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import path from 'node:path';
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'OmgRod',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			pagination: false,
-			// sidebar: [
-			// 	{
-			// 		label: 'Minecraft',
-			// 		slug: 'minecraft'
-			// 	},
-			// ]
-			components: {
-				SiteTitle: './src/starlightOverrides/SiteTitle.astro',
-			},
-		}),
-	],
-	vite: {
-		resolve: {
-			alias: {
-				'@': path.resolve('./src'),
-			}
-		}
-	},
+    integrations: [starlight({
+        title: 'OmgRod',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        pagination: false,
+        // sidebar: [
+        // 	{
+        // 		label: 'Minecraft',
+        // 		slug: 'minecraft'
+        // 	},
+        // ]
+        components: {
+            SiteTitle: './src/starlightOverrides/SiteTitle.astro',
+        },
+		}), sentry(), spotlightjs()],
+    vite: {
+        resolve: {
+            alias: {
+                '@': path.resolve('./src'),
+            }
+        }
+    },
 
 });
